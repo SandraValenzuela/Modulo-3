7131189a3b6408e4aaa951aa382d14f5eb32ba16
Sandra Valenzuela <sandravalenzuela.fi@gmail.com>
2022-05-12 16:53:15 -0400

    cambios1

---
+++ js/M03C04_Ejercicios.js
@@ -0,0 +1,37 @@
+let foto=document.querySelector("img");
+//let foto2 = $("img");
+let btn=document.querySelector("#boton");
+//let btn=$("#boton");
+
+console.log(foto);
+//console.log(foto2);
+
+console.log(btn);
+
+//agregar click
+btn.addEventListener("click", function(e){
+    $.getJSON("https://dog.ceo/api/breeds/image/random", function(data){
+        let obj=data;
+        let URLimagen=data.message;
+
+        foto.setAttribute("src", URLimagen);
+    })
+
+})
+
+//--------------------------------------------------------------------------------------------------------------
+// Ejercicio Gato // Boton gato
+let fotoGato=document.querySelector("#imgGato");
+let btn1=document.querySelector("#botonGato");
+console.log(fotoGato);
+console.log(btn1);
+
+btn1.addEventListener("click", function(g){
+    $.getJSON("https://api.thecatapi.com/v1/images/search", function(data){
+        let obj1=data;
+        let URLimagen=data.message;
+
+        fotoGato.setAttribute("src", URLimagen);
+    })
+
+})
